5a85365fbe8bc5d97c5df57b03d6c70a4e082f97
Kieran McGrady <kieranjmcgrady@gmail.com>
2019-06-24 14:08:20 +0100

    Implement all callbacks and log errors

--- MediationSample/Assets/MediationSample.cs
+++ MediationSample/Assets/MediationSample.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Tapdaq;
@@ -10,29 +11,101 @@ public class MediationSample : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		AdManager.Init ();
+		AdManager.Init ();		
 	}
-	
+
 	// Subscribe to Tapdaq events
 	private void OnEnable () {
 		TDCallbacks.TapdaqConfigLoaded += LoadConfig;
+		TDCallbacks.TapdaqConfigFailedToLoad += FailedToLoadConfig;
 		TDCallbacks.AdAvailable += AdAvailable;
+		TDCallbacks.AdNotAvailable += AdNotAvailable;
+		TDCallbacks.AdWillDisplay += AdWillDisplay;
+		TDCallbacks.AdDidDisplay += AdDidDisplay;
+		TDCallbacks.AdDidFailToDisplay += AdDidFailToDisplay;
+		TDCallbacks.AdClicked += AdClicked;
+		TDCallbacks.AdClosed += AdClosed;
+		TDCallbacks.AdError += AdError;
+		TDCallbacks.RewardVideoValidated += RewardVideoValidated;
+		TDCallbacks.CustomEvent += CustomEvent;
 	}
 
 	// Unsubscribe from Tapdaq events
 	private void OnDisable () {
 		TDCallbacks.TapdaqConfigLoaded -= LoadConfig;
+		TDCallbacks.TapdaqConfigFailedToLoad -= FailedToLoadConfig;
 		TDCallbacks.AdAvailable -= AdAvailable;
+		TDCallbacks.AdNotAvailable -= AdNotAvailable;
+		TDCallbacks.AdWillDisplay -= AdWillDisplay;
+		TDCallbacks.AdDidDisplay -= AdDidDisplay;
+		TDCallbacks.AdDidFailToDisplay -= AdDidFailToDisplay;
+		TDCallbacks.AdClicked -= AdClicked;
+		TDCallbacks.AdClosed -= AdClosed;
+		TDCallbacks.AdError -= AdError;
+		TDCallbacks.RewardVideoValidated -= RewardVideoValidated;
+		TDCallbacks.CustomEvent -= CustomEvent;
 	}
 
+    //Callback handlers
 	private void LoadConfig() {
 		//SDK BOOT COMPLETE
+		Debug.Log("Tapdaq config loaded");
+	}
+
+    private void FailedToLoadConfig(TDAdError e) {
+		LogError(e);
 	}
 
 	private void AdAvailable(TDAdEvent e) {
-		Debug.Log ("AdAvailable Type: " + e.adType + " - Tag: " + e.tag);
+		Debug.Log("AdAvailable Type: " + e.adType + " - Tag: " + e.tag);
 	}	
 
+    private void AdNotAvailable(TDAdEvent e) {
+		LogError(e.error);
+	}
+
+	private void AdWillDisplay(TDAdEvent e) {
+		Debug.Log("AdWillDisplay Type: " + e.adType + " - Tag: " + e.tag);
+	}
+
+	private void AdDidDisplay(TDAdEvent e) {
+		Debug.Log("AdDidDisplay Type: " + e.adType + " - Tag: " + e.tag);
+	}
+
+	private void AdDidFailToDisplay(TDAdEvent e) {	
+		LogError(e.error);
+	}
+
+	private void AdClicked(TDAdEvent e) {
+		Debug.Log("AdClicked Type: " + e.adType + " - Tag: " + e.tag);
+	}
+
+	private void AdClosed(TDAdEvent e) {
+		Debug.Log("AdClosed Type: " + e.adType + " - Tag: " + e.tag);
+	}
+
+	private void AdError(TDAdEvent e) {
+		LogError(e.error);
+	}
+
+    private void RewardVideoValidated(TDVideoReward e) {
+		Debug.Log("RewardValidated name: " + e.RewardName + " - Tag: " + e.Tag + " - amount: " + e.RewardAmount);
+	}
+
+    private void CustomEvent(Dictionary<string, object> dictionary) {
+		Debug.Log("CustomEvent dictionary:" + dictionary);
+	}
+
+	// Error logging
+	private void LogError(TDAdError e) {
+        Debug.Log("Code: " + e.code + " Message: " + e.message);
+        foreach(KeyValuePair<String, List<TDAdError>> entry in e.subErrors) {
+            Debug.Log("Code: " + e.code + "Message: " + e.message);
+        }
+	}
+
+
+	// Load Ads
 	public void LoadStaticInterstitial() {
 		AdManager.LoadInterstitial (mTag);
 	}
@@ -73,3 +146,5 @@ public class MediationSample : MonoBehaviour {
 		mTag = placementField.text;
     }
 }
+
+
