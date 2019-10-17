6cda0e93d7e7bf442e8edf40cbf3d202ee2f64db
Dominic Roberts <dom.roberts@tapdaq.com>
2019-10-17 19:23:52 +0100

    Updated Mediation sample to 7.3.1

--- MediationSample/Assets/Plugins/Tapdaq/Scripts/TDObjects/TDSettings.cs
+++ MediationSample/Assets/Plugins/Tapdaq/Scripts/TDObjects/TDSettings.cs
@@ -7,7 +7,7 @@ namespace Tapdaq {
 	public class TDSettings : ScriptableObject {
 		private static TDSettings instance;
 
-		public const string pluginVersion = "unity_7.3.0";
+		public const string pluginVersion = "unity_7.3.1";
 		
 		public string ios_applicationID = "";
 		public string ios_clientKey = "";
