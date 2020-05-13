f172d12794e3cedf9eed58499bb7fa57de605912
Dominic Roberts <dom.roberts@tapdaq.com>
2020-05-13 17:26:52 +0100

    Increased Tapdaq Android SDK version to 7.6.0-rc2

--- MediationSample/Assets/Plugins/Tapdaq/Editor/TDDependenciesResolver.cs
+++ MediationSample/Assets/Plugins/Tapdaq/Editor/TDDependenciesResolver.cs
@@ -27,7 +27,7 @@ using System.IO;
 [InitializeOnLoad]
 public class TDDependencies : AssetPostprocessor
 {
-	private static string TAPDAQ_ANDROID_VERSION = "7.6.0";
+	private static string TAPDAQ_ANDROID_VERSION = "7.6.0-rc2";
 	private static string TAPDAQ_IOS_VERSION = "7.6.0";
 
     private static string DEPDENCIES_DIRECTORY = "/Plugins/Tapdaq/Editor/TapdaqDependencies.xml";
